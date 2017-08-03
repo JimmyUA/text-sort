5223782e0b7371a54fa659cec408624fde769752
jimmy <sergii.pryhodko@gmail.com>
2017-08-03 05:17:13 +0300

    ToStrong fixed

--- text-sort/src/main/java/com/sergey/prykhodko/textParts/Word.java
+++ text-sort/src/main/java/com/sergey/prykhodko/textParts/Word.java
@@ -2,5 +2,13 @@ package com.sergey.prykhodko.textParts;
 
 public class Word implements SentenceUnit{
     private String word;
-    private static final String WORD_REG_EX = "";
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    @Override
+    public String toString() {
+        return word;
+    }
 }
