b806d6ee1673eecfeffe597fd24908682c205215
jimmy <sergii.pryhodko@gmail.com>
2017-08-03 05:19:47 +0300

    toString constructor and others

--- text-sort/src/main/java/com/sergey/prykhodko/textParts/PunctuationUnit.java
+++ text-sort/src/main/java/com/sergey/prykhodko/textParts/PunctuationUnit.java
@@ -2,4 +2,13 @@ package com.sergey.prykhodko.textParts;
 
 public class PunctuationUnit implements SentenceUnit{
     private char unit;
+
+    public PunctuationUnit(char unit) {
+        this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "" + unit;
+    }
 }
