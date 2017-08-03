fa5ad57a69a5b84d0a467b643bdf40e2d752227f
jimmy <sergii.pryhodko@gmail.com>
2017-08-03 05:18:54 +0300

    fixed to string and added list of words and list of punctual units

--- text-sort/src/main/java/com/sergey/prykhodko/textParts/Sentence.java
+++ text-sort/src/main/java/com/sergey/prykhodko/textParts/Sentence.java
@@ -1,14 +1,48 @@
 package com.sergey.prykhodko.textParts;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Sentence {
     private String sentence;
     private Queue<SentenceUnit> sentenceInUnitRepresentation;
+    private List<Word> words;
+    private List<PunctuationUnit> punctuationUnits;
 
     public Sentence(String sentence) {
         this.sentence = sentence;
         sentenceInUnitRepresentation = new ArrayDeque<>();
+        words = new ArrayList<>();
+        punctuationUnits = new ArrayList<>();
+
+        getAllWords();
+        getAllPunctuatioUnits();
+    }
+
+    private void getAllPunctuatioUnits() {
+        Pattern punctuationUnitPattern = Pattern.compile(SentenceUnit.PUNCTUATION_UNIT_REG_EX);
+        Matcher punctuationUnitMatcher = punctuationUnitPattern.matcher(sentence);
+
+        while (punctuationUnitMatcher.find()){
+            punctuationUnits.add(new PunctuationUnit(punctuationUnitMatcher.group().charAt(0)));
+        }
+    }
+
+    private void getAllWords(){
+        Pattern wordPattern = Pattern.compile(SentenceUnit.WORD_REG_EX);
+        Matcher wordMatcher = wordPattern.matcher(sentence);
+
+        while (wordMatcher.find()){
+            words.add(new Word(wordMatcher.group()));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return sentence;
     }
 }
