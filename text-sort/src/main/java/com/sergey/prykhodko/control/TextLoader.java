b2927df7bb3a39c54ba2ca0066c2ee0dbfbc2bd5
jimmy <sergii.pryhodko@gmail.com>
2017-08-03 03:05:39 +0300

    Text loader implemented

--- text-sort/src/main/java/com/sergey/prykhodko/control/TextLoader.java
+++ text-sort/src/main/java/com/sergey/prykhodko/control/TextLoader.java
@@ -1,8 +1,34 @@
 package com.sergey.prykhodko.control;
 
+import com.sergey.prykhodko.textParts.Sentence;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextLoader {
 
-    public void getTextFromFile(){
+    public List<Sentence> getTextFromFile(File fileWithGoalText){
+        List<Sentence> sentences = new ArrayList<>();
 
+        try(InputStream in = new FileInputStream(fileWithGoalText)){
+            int i;
+            int count = 0;
+            int minLength = 2;
+
+            StringBuilder builder = new StringBuilder();
+
+            while ((i = in.read()) != -1){
+                builder.append((char)i);
+                if (i == 46 && builder.length() > minLength){
+                    sentences.add(new Sentence(builder.toString().trim()));
+                    builder = new StringBuilder();
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sentences;
     }
 }
