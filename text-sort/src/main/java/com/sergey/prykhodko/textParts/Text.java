6e3bd80bd5728809bbc70bb792733d8553344349
jimmy <sergii.pryhodko@gmail.com>
2017-08-09 21:14:54 +0300

    done

--- text-sort/src/main/java/com/sergey/prykhodko/textParts/Text.java
+++ text-sort/src/main/java/com/sergey/prykhodko/textParts/Text.java
@@ -3,13 +3,18 @@ package com.sergey.prykhodko.textParts;
 import java.util.List;
 
 public class Text {
-    private List<Sentence> text;
+    private List<Sentence> sentences;
+    private String text;
 
-    public List<Sentence> getSentencesAsList() {
-        return text;
+    public void setText(String text) {
+        this.text = text;
     }
 
-    public void setText(List<Sentence> text) {
-        this.text = text;
+    public List<Sentence> getSentencesAsList() {
+        return sentences;
+    }
+
+    public void setSentences(List<Sentence> sentences) {
+        this.sentences = sentences;
     }
 }
